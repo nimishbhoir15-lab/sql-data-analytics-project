8a46903c296689f19338063d5f662167fe20b9c0
Nimish <nimishbhoir15@gmail.com>
2026-09-10 18:15:16 +0530

    Create database setup script with tables and data import
    This script sets up the database schema and imports initial data for the DataWarehouseAnalytics project.

---
+++ scripts/01_database_setup.sql
@@ -0,0 +1,76 @@
+
+
+-- Use database
+USE DataWarehouseAnalytics;
+
+-------------------------------------------------------------
+-- Create Tables
+-------------------------------------------------------------
+
+CREATE TABLE dim_customers (
+    customer_key INT,
+    customer_id INT,
+    customer_number VARCHAR(50),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    country VARCHAR(50),
+    marital_status VARCHAR(50),
+    gender VARCHAR(50),
+    birthdate DATE,
+    create_date DATE
+);
+
+CREATE TABLE dim_products (
+    product_key INT,
+    product_id INT,
+    product_number VARCHAR(50),
+    product_name VARCHAR(50),
+    category_id VARCHAR(50),
+    category VARCHAR(50),
+    subcategory VARCHAR(50),
+    maintenance VARCHAR(50),
+    cost INT,
+    product_line VARCHAR(50),
+    start_date DATE
+);
+
+CREATE TABLE fact_sales (
+    order_number VARCHAR(50),
+    product_key INT,
+    customer_key INT,
+    order_date DATE,
+    shipping_date DATE,
+    due_date DATE,
+    sales_amount INT,
+    quantity TINYINT,
+    price INT
+);
+
+
+-------------------------------------------------------------
+-- Import Data
+-------------------------------------------------------------
+
+
+
+LOAD DATA LOCAL INFILE 'C:\Users\NIMISH\Downloads\sql-data-analytics-project\sql-data-analytics-project\scripts'
+INTO TABLE dim_customers
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\Users\NIMISH\Downloads\sql-data-analytics-project\sql-data-analytics-project\scripts'
+INTO TABLE dim_products
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:\Users\NIMISH\Downloads\sql-data-analytics-project\sql-data-analytics-project\scripts'
+INTO TABLE fact_sales
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
